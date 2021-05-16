e6b9d2cdd868abd8032093eac95d0521d82972da
jgr <grobles.javier@gmail.com>
2021-05-16 12:36:56 +0200

    refactor(config): removed unnecessary imports

--- src/main/java/com/jgr/taskmanager/configuration/TaskConfig.java
+++ src/main/java/com/jgr/taskmanager/configuration/TaskConfig.java
@@ -1,12 +1,7 @@
 package com.jgr.taskmanager.configuration;
 
-import com.jgr.taskmanager.service.TaskService;
-import com.jgr.taskmanager.service.impl.TaskServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
