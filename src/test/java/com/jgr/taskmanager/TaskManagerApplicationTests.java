878c2285122e9ddba9bd4b68e271012b1eceed80
JavierGrobles <grobles.javier@gmail.com>
2021-05-18 22:19:49 +0200

    test: add mock for repository

--- src/test/java/com/jgr/taskmanager/TaskManagerApplicationTests.java
+++ src/test/java/com/jgr/taskmanager/TaskManagerApplicationTests.java
@@ -2,12 +2,23 @@ package com.jgr.taskmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import com.jgr.taskmanager.repository.TaskRepository;
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 class TaskManagerApplicationTests {
-
+@MockBean private TaskRepository taskRepository;
 	@Test
 	void contextLoads() {
 	}
-
+	 @Configuration
+	static class TestConfig {
+		@Bean
+		TaskRepository taskRepository() {
+		  return mock(TaskRepository.class);
+		}
+	}
 }
